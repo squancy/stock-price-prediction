3db3dbb3a67f3c29c291f309aeb40c2abf679c4d
Mark Frankli <mark@pearscom.com>
2025-05-22 02:40:05 +0200

    finished

--- consts.py
+++ consts.py
@@ -2,5 +2,5 @@ STOP_LOSS_PCT = 0.05
 TAKE_PROFIT_PCT = 0.1
 INIT_CASH = 10 ** 6
 NUM_OF_PLOTS = 5 
-IS_TEST = True # only a small portion of the dataset is used in test mode
-TEST_NUM = 100 # set it to a small int so it avoids the overhead of loading the entire dataset
+IS_TEST = False # only a small portion of the dataset is used in test mode
+TEST_NUM = 250 # set it to a small int so it avoids the overhead of loading the entire dataset
